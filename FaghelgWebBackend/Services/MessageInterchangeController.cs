c0b3b5123dba4b560f4b17ffe69cf7644cb495bb
Anders Asperheim <anders@asperheim.no>
2015-09-19 11:50:03 +0200

    Added controller for message interchange and service for messages

---
+++ FaghelgWebBackend/Services/MessageInterchangeController.cs
@@ -0,0 +1,19 @@
+﻿using FaghelgWebBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FaghelgWebBackend.Services
+{
+    public class MessageInterchangeController : ApiController
+    {
+        // POST: api/MessageInterchange
+        public void Post(Message message)
+        {
+
+        }
+    }
+}
