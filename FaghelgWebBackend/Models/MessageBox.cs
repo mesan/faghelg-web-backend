dbe21cc373e2c4c69e186d2e884610bbdc068c12
Anders Asperheim <anders@themoon.com>
2015-09-25 12:41:07 +0200

    Added messagebox class

---
+++ FaghelgWebBackend/Models/MessageBox.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FaghelgWebBackend.Models
+{
+    public class MessageBox
+    {
+        Guid owner { get; set; }
+        IEnumerable messages { get; set; }
+        int messageCount { get; set; }
+        int pages { get; set; }
+    }
+}
